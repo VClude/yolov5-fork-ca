2585361fff421341228db1298deef5d0f7409f32
Google Colab VM <vclude@gmail.com>
2025-09-27 10:45:23 +0000

    update sync

--- runtrain.py
+++ runtrain.py
@@ -23,19 +23,19 @@
 import roboflow
 rf = roboflow.Roboflow(api_key="AkZNNZBdY0QrIN62pzp0")
 project = rf.workspace("kelenjar-minyak").project("kelenjar-minyak-yolo-standard-ke3j5")
-dataset = project.version(3).download("yolov5")
+dataset = project.version(4).download("yolov5")
 
 import subprocess
 subprocess.run([
     "python", "train.py",
-    "--img", "640",
-    "--batch", "64",
+    "--img", "960",
+    "--batch", "-1",
     "--epochs", "200",
     f"--data={dataset.location}/data.yaml",
     "--weights", "yolov5s.pt",
-    "--hyp", "hyp.dcn.precision640.yaml",
-    "--cfg", "yolov5s-cav2-eca.yaml",
-    "--name", "km-v5s-ca-eca-640-single-class",
+    "--hyp", "hyp.ca.yaml",
+    "--cfg", "yolov5s-cav2-cbam.yaml",
+    "--name", "km-v5s-ca-cbam-960",
     "--device", "0",
     "--optimizer", "AdamW", 
     "--workers", "32",
